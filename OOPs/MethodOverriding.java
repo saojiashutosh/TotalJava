6c0b5032cd789e17353e7d6010d63c4fab458424
Ashutosh Saoji <ashutoshsaoji1@gmail.com>
2024-01-26 17:07:19 +0530

    commit

---
+++ OOPs/MethodOverriding.java
@@ -0,0 +1,20 @@
+package TotalJava.OOPs;
+
+public class MethodOverriding {
+    public static void main(String[] args){
+        Dear d = new Dear();
+        d.eats();
+    }
+}
+
+class Animal{
+    void eats(){
+        System.out.println("Eats Flesh");
+    }
+}
+
+class Dear extends Animal{
+    void eats(){
+        System.out.println("Eats Grass");
+    }
+}
