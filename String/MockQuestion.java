f48819708d29c7438350aa2f5c601f60cb8f2b81
Ashutosh Saoji <ashutoshsaoji1@gmail.com>
2024-02-05 22:26:57 +0530

    commit

--- String/MockQuestion.java
+++ String/MockQuestion.java
@@ -1,26 +1,15 @@
 package TotalJava.String;
-
+/// Longest Substring of String
 public class MockQuestion {
     public static void main(String[] args ){
         String str = "Hello World , I am Ashutosh";
-        str = str.replaceAll("\\s","") ;
-        System.out.println(str);
-        str = str.replaceAll(",","") ;
-        System.out.println(str);
 
-        for(int i = 0;i<str.length();i++){
-            int start = i ;
-            for(int j = 1;j<str.length();j++){
-                int end = j;
-                for(int k=start;k<=end;k++){
-                    System.out.print(str.charAt(k));
-                }
-                System.out.println();
-            }
-        }
+        str = str.replaceAll("\\s","") ;
+        str = str.replaceAll(",","") ;
 
         String longestSubstring = findLongestSubstring(str);
-        System.out.println("Longest substring without repeating characters: " + longestSubstring);
+        System.out.println("Longest substring of a String : " + longestSubstring);
+
     }
 
     private static String findLongestSubstring(String str) {
@@ -34,10 +23,7 @@ public class MockQuestion {
             if (end - start + 1 > maxLength) {
                 maxLength = end - start + 1;
                 longestStart = start;
-                System.out.print(str.charAt(start));
-                System.out.print(str.charAt(end));
             }
-            System.out.println();
         }
         // Extract the longest substring
         return str.substring(longestStart, longestStart + maxLength);
